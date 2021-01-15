dc30b630276994428f3b88a36bb595271cd9b5b7
jayakody.ml <jayakody.ml@gmail.com>
2021-01-15 23:17:44 +0530

    Student Dashboard view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,36 @@
 
 
-import React from 'react'
+// import React from 'react'
 
-import './css/log.css'
-
-
-
-const Home = () => {
-    return (
-        <div className="Container">
-            <h4 className="center">HOME</h4>
-            <div>
-
-                {
-                    <div>
-
-                        <div className="content">
-      <span>Welcome to LSMS, Faculty of Engineering,University Of Ruhuna
-      </span>
-                        </div>
-                    </div>
-                }
-
-            </div>
-        </div>
-
-    )
-
-}
+// import './css/log.css'
 
 
-export default Home
+
+// const Home = () => {
+//     return (
+//         <div className="Container">
+//             <h4 className="center">HOME</h4>
+//             <div>
+
+//                 {
+//                     <div>
+
+//                         <div className="content">
+//       <span>Welcome to LSMS, Faculty of Engineering,University Of Ruhuna
+//       </span>
+//                         </div>
+//                     </div>
+//                 }
+
+//             </div>
+//         </div>
+
+//     )
+
+// }
+
+
+// export default Home
 
 
 
