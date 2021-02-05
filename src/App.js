b539add8106c0fb85a17d261a786046f8a60d3fe
unknown <you@example.com>
2021-02-05 14:31:27 +0530

    modified login page and create table and calendar veiw

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-// import Home from './components/Home'
 import { Route, BrowserRouter } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -9,6 +8,9 @@ import LecturerRegisterForm from "./components/LecturerRegisterForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StudentDashboard from './components/student/StudentDashboard';
 import StudentProfile from './components/student/StudentProfile';
+import Home from './components/Home'
+import FirstPage from './components/FirstPage'
+
 
 class App extends Component {
     render() {
@@ -16,18 +18,21 @@ class App extends Component {
             <BrowserRouter>
                 <div className="App">
                     <Navbar />
-                    <Route exact path='/' component={Login}/>
+                    <Route exact path='/' component={FirstPage}/>
                     <Route path='/student' component={StudentDashboard}/>
-                    {/* <Route path='/' component={Home}/> */}
-                    <Route path='/Register' component={Register} />
+                   <Route path='/Register' component={Register} />
                     <Route path='/LecturerRegisterForm' component={LecturerRegisterForm} />
                     <Route path='/StudentRegisterForm' component={StudentRegisterForm} />
                     <Route path= '/Student/Profile' component={StudentProfile}/>
+                    <Route path= '/Home' component={Home}/>
+                    <Route path= '/Login' component={Login}/>
+
                 </div>
             </BrowserRouter>
         );
     }
 }
+
 
 export default App;
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,39 +4,35 @@ import './css/log.css'
 import icon from './../images/icon.jpg'
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
 import Checkbox from "@material-ui/core/Checkbox";
-import {Image} from "react-bootstrap";
+import {Image,Button} from "react-bootstrap";
+import Grid from 'react-bootstrap/Container'
 
 const Login = () => {
     return (
-        <div className="split left">
 
-                    <div className="p1">
-                        <h3><u>LECTURE SCHEDULE MANAGEMENT SYSTEM</u></h3>
-                    </div>
 
+        <div>
+            <div className="p1">
+                <h3><u>LECTURE SCHEDULE MANAGEMENT SYSTEM</u></h3>
+            </div>
             <div className="centered img">
                 <Image img src={icon} alt="icon"/>
-
-
                         {/* properties set in the login page */}
-                        <div className="Container">
-                            <div className="content">
-                                            <TextField
+                        <table>
+                                <tr>        <TextField
                                             variant="outlined"
                                             margin="normal"
                                             required
-                                            fullWidth
+                                         fullWidth
                                             id="uname"
                                             label="User Name"
                                             name="uname"
                                             autoComplete="uname"
                                             autoFocus
-                                            />
+                                            /></tr>
 
-                                           <TextField
+                                <tr>     <TextField
                                             variant="outlined"
                                             margin="normal"
                                             required
@@ -46,9 +42,9 @@ const Login = () => {
                                             type="password"
                                             id="password"
                                             autoComplete="current-password"
-                                             />
+                                             />  </tr>
 
-                                            <FormControlLabel
+                                <FormControlLabel
                                             control={<Checkbox value="remember" color="#91beeb" />}
                                             label="Remember me"
                                             />
@@ -75,39 +71,9 @@ const Login = () => {
                                                              </Link></p>
                                                </Grid>
                                            </Grid>
-                            </div>
-                        </div>
+                            </table>
                       </div>
-
-
-
-
-
-            {/* right part of the page setting*/}
-            <div className="split right">
-
-
-                  <div className="centered">
-
-                      <div class="p1">
-                           <div id="movetxt"><p class="groove">LSMS   <br/>
-                             FACULTY OF ENGINEERING<br/>
-                             UNIVERSITY OF RUHUNA</p>
-                           </div>
-                      </div>
-                  </div>
-
-                  <div className="topright">
-                                <dl>
-                                    <dt><b>Contact Info : </b></dt>
-                                    <dd><b>Website :<a href="http://www.eng.ruh.ac.lk/"> eng.ruh.ac.lk </a></b></dd>
-                                    <dd><b>Address : Hapugala, Galle, Sri Lanka</b></dd>
-                                    <dd><b>Tel: +(94)0 91 2245765-8</b></dd>
-                                    <dd><b> Fax:+(94)0 91 2245762</b></dd>
-                                </dl>
-                  </div>
-            </div>
-        </div>
+</div>
 
     );
 
