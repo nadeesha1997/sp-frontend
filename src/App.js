be4a73d147f3ed682e149e7f2dd8d9770a752f31
Awanthi96 <jayasundarakasunika@gmail.com>
2021-03-26 15:24:25 +0530

    logging alert modified

--- src/App.js
+++ src/App.js
@@ -21,10 +21,17 @@ import TimeTable from "./components/TimeTable/TimeTable";
 import SubjectList from "./components/TimeTable/SubjectList";
 import ModuleDrop from "./components/TimeTable/ModuleDrop";
 import ModuleEnrolement from "./components/admin/moduleEnrolement";
+
+
 import StudentUpdateProfile from "./components/student/StudentUpdateProfile";
+import LecturerUpdateProfile from "./components/lecturer/LecturerUpdateProfile";
+import AdminUpdateProfile from "./components/admin/AdminUpdateProfile";
 
 import HomepageNavbar from "./components/TimeTable/HomepageNavbar";
-import StudentTimetable from "./components/student/StudentTimetable";
+
+import StudentTimetable from "./components/student/StudentTimetable"
+import AdminTimetable from "./components/admin/AdminTimetable"
+import LecturerTimetable from "./components/lecturer/LecturerTimetable"
 // /*
 // import ProfileNew from "./components/student/ProfileNew";
 // */
@@ -41,12 +48,15 @@ class App extends Component {
 
                     <Route path='/Register' component={Register} />
                     <Route path='/Dashboard' component={Dashboard} />
+
                     <Route path='/LecturerRegisterForm' component={LecturerRegisterForm} />
                     <Route path='/StudentRegisterForm' component={StudentRegisterForm} />
                     <Route path='/AdminRegisterForm' component={AdminRegisterForm} />
+
                     <Route path= '/Student/StudentProfile' component={StudentProfile}/>
                     <Route path= '/Lecturer/LecturerProfile' component={LecturerProfile}/>
                     <Route path= '/Admin/AdminProfile' component={AdminProfile}/>
+
                     <Route path= '/Home' component={Home}/>
                     <Route path= '/Login' component={Login}/>
                     <Route path= '/SubjectList' component={SubjectList}/>
@@ -54,8 +64,17 @@ class App extends Component {
                     <Route path= '/ModuleDrop' component={ModuleDrop}/>
                     <Route path='/moduleenrolement' component={ModuleEnrolement}/>
 
-                    <Route path= '/Student/UpdateProfile' component={StudentUpdateProfile}/>
+                    <Route path= '/Student/StudentUpdateProfile' component={StudentUpdateProfile}/>
+                    <Route path= '/Lecturer/LecturerUpdateProfile' component={LecturerUpdateProfile}/>
+                    <Route path= '/Admin/AdminUpdateProfile' component={AdminUpdateProfile}/>
+
+
                     <Route path='/student/StudentTimetable' component={StudentTimetable}/>
+                    <Route path='/lecturer/LecturerTimetable' component={LecturerTimetable}/>
+                    <Route path='/admin/AdminTimetable' component={AdminTimetable}/>
+
+
+
                 </div>
             </BrowserRouter>
         );
