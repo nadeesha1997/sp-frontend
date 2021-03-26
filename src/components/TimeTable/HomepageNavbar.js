9a8f5edf4e6260db4e97f016c86d2b8461178c8e
Madhushika97 <b.d.m.niroshanee@gmail.com>
2021-03-26 09:12:48 +0530

    modified

--- src/components/TimeTable/HomepageNavbar.js
+++ src/components/TimeTable/HomepageNavbar.js
@@ -25,45 +25,44 @@ class HomepageNavbar extends Component{
     render() {
         return (
 
-            <div className="App">
+<div className="App">
 
-                <div className="navbar-color">
-                    <nav  className="navbar navbar-expand-lg navbar-fixed-top" >
-                        <Image img src={logo2} alt="logo"  width={300} height={150}/>
-                        <div className="text1">
-                            <ul>
-                                <li>
-                                    <h5><b>FACULTY OF ENGINEERING</b></h5>
-                                    <h5><b>UNIVERSITY OF RUHUNA</b></h5>
-                                    <h5><b>LECTURE SCHEDULE MANAGEMENT SYSTEM</b></h5>
-                                </li>
-                            </ul>
-                            {/* <ul>
+           <div className="navbar-color">
+                <nav  className="navbar navbar-expand-lg navbar-fixed-top" >
+                    <Image img src={logo2} alt="logo"  width={300} height={150}/>
+                    <div className="text">
+                        <ul>
+                            <li>
+                                <h5><b>FACULTY OF ENGINEERING</b></h5>
+                                <h5><b>UNIVERSITY OF RUHUNA</b></h5>
+                                <h5><b>LECTURE SCHEDULE MANAGEMENT SYSTEM</b></h5>
+                            </li>
+                        </ul>
+                           {/* <ul>
                             <li>
                                 <Button  style={{width:90,backgroundColor:'#7047ba',marginTop:0,marginLeft:0,marginRight:10,fontFamily:"Arial",color:"black"}} type='submit' >
+
                                 <Link to=""><b>Home</b>  </Link>
                             </Button>
                                 <Button  className="btn-link" style={{width:90,backgroundColor:'#7047ba',marginTop:0,marginLeft:0,marginRight:10,fontFamily:"Arial"}} type='submit' >
+
                                     <Link to="/student/Profile"><b>Profile</b>  </Link>
                                 </Button>
                                 <Button  style={{width:90,backgroundColor:'#7047ba',marginTop:0,marginLeft:0,marginRight:10,fontFamily:"Arial"}} type='submit' >
+
                                     <Link to="./index"><b>Map</b>  </Link>
                                 </Button>
+
                             </li>
                         </ul>*/}
-
+                    </div>
+                    <div className="cols-lg-12 col-md-4 col-sm-12">
+                        <div className="locate">
+                        <CalendarNew updateDate={this.updateDate}/>
                         </div>
-                        {/* <div className="cols-lg-12 col-md-4 col-sm-12">*/}
-
-                            {/*<div>
-                                <CalendarNew updateDate={this.updateDate}/>
-                            </div>*/}
-
-                        {/* </div>*/}
-
-
-                    </nav>
-                </div>
+                    </div>
+                </nav>
+            </div>
 
             </div>
         );
